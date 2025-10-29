a6e3277347d46f9751293f9cee614ee55644105c
Juliette Bénon <62658468+jbenon@users.noreply.github.com>
2025-10-29 16:06:44 +0100

    implement exit

--- app/main.py
+++ app/main.py
@@ -2,13 +2,26 @@ import sys
 
 
 def main():
-    while True:
+    exit = False
+    while not exit:
         # Print a prompt
         sys.stdout.write("$ ")
 
-        # Handle invalid commands
+        # Parse command
         command = input()
-        sys.stdout.write(f"{command}: command not found\n")
+        command_parsed = command.split(" ")
+        command = command_parsed[0]
+        match len(command_parsed):
+            case 1:
+                param = None
+            case 2:
+                param = int(command_parsed[1])
+
+        # Exit or invalid
+        if command == "exit" and param == 0:
+            exit = True
+        else:
+            sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
